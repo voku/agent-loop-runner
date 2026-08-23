bb384a10054b5721afb37836d703ce1fad85dc75
Lars Moelleken <lars@moelleken.org>
2026-08-23 19:55:59 +0200

    feat(process): project allowlisted host environment

---
+++ src/Process/EnvironmentProjector.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace voku\AgentLoopRunner\Process;
+
+final readonly class EnvironmentProjector
+{
+    /**
+     * @param list<non-empty-string> $allowlist
+     * @return array<string, string>
+     */
+    public function project(array $allowlist): array
+    {
+        $environment = [];
+        foreach ($allowlist as $name) {
+            $value = getenv($name);
+            if (is_string($value)) {
+                $environment[$name] = $value;
+            }
+        }
+
+        return $environment;
+    }
+}
