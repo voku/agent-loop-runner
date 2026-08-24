c38027a8315d436868fd9b24150cab801242eac6
Lars Moelleken <lars@moelleken.org>
2026-08-24 07:43:50 +0200

    feat(execution): expose environment-aware gateway port

--- src/Execution/ExecutionGatewayPort.php
+++ src/Execution/ExecutionGatewayPort.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 namespace voku\AgentLoopRunner\Execution;
+use voku\AgentLoop\Execution\ExecutionEnvironmentObservation;
 use voku\AgentLoop\Execution\ExecutionProjection;
 use voku\AgentLoop\Execution\StageExecutionBundle;
 use voku\AgentLoop\Execution\StageResult;
@@ -9,6 +10,7 @@ interface ExecutionGatewayPort
 {
     public function projection(string $taskId): ExecutionProjection;
     public function prepareStage(string $taskId, string $stageId): StageExecutionBundle;
+    public function prepareStageForEnvironment(string $taskId, string $stageId, ExecutionEnvironmentObservation $observation): StageExecutionBundle;
     public function submitStageResult(StageResult $result): ExecutionProjection;
     public function runDeterministicStage(string $taskId, string $stageId): ExecutionProjection;
 }
